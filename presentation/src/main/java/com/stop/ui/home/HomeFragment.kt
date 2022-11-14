ee7bf4d19b13c4044646c9742f3982b92994b1e0
DoDoBest <av449@naver.com>
2022-11-14 18:48:17 +0900

    feat: 경로 검색 화면으로 가는 nav graph 설정

--- presentation/src/main/java/com/stop/ui/home/HomeFragment.kt
+++ presentation/src/main/java/com/stop/ui/home/HomeFragment.kt
@@ -5,18 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.stop.R
 
 class HomeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_move_to_search).setOnClickListener {
+            findNavController().navigate(R.id.action_home_fragment_to_search_fragment)
+        }
     }
 }
