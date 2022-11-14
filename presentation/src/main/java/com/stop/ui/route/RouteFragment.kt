9b70ab188175b3cad5e932aaaa5fd99df7a82714
DoDoBest <av449@naver.com>
2022-11-14 18:47:21 +0900

    chore: 경로 검색 화면 Fragment 임시 생성

---
+++ presentation/src/main/java/com/stop/ui/route/RouteFragment.kt
@@ -0,0 +1,21 @@
+package com.stop.ui.route
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.stop.R
+
+class RouteFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_route, container, false)
+    }
+}
