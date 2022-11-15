17023c968678f3a6c47cb262538a3ce979b38923
DoDoBest <av449@naver.com>
2022-11-15 13:29:14 +0900

    feat: 경로 검색 화면에 전달받은 출발지, 도착지를 표시하는 UI 만들기

--- presentation/src/main/java/com/stop/ui/route/RouteFragment.kt
+++ presentation/src/main/java/com/stop/ui/route/RouteFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
 import androidx.navigation.fragment.navArgs
 import com.stop.R
+import com.stop.databinding.FragmentRouteBinding
+import com.stop.model.route.OrderType
 
 class RouteFragment : Fragment() {
+
+    private var _binding: FragmentRouteBinding? = null
+    private val binding: FragmentRouteBinding
+        get() = _binding!!
 
     private val args: RouteFragmentArgs by navArgs()
 
@@ -17,13 +23,32 @@ class RouteFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_route, container, false)
+        _binding = FragmentRouteBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<TextView>(R.id.text_view_result).text = args.routeInfo.toString()
+        setStartAndDestinationText()
+        setDropDownMenu()
     }
 
+    private fun setDropDownMenu() {
+        val options = OrderType.values().map { it.typeName }
+        val adapter = ArrayAdapter(requireContext(), R.layout.order_list_item, options)
+        binding.autoCompleteTextViewOrderType.setText(options.first())
+        binding.autoCompleteTextViewOrderType.setAdapter(adapter)
+    }
+
+    private fun setStartAndDestinationText() {
+        binding.textInputEditTextOrigin.setText(args.routeInfo.originName)
+        binding.textInputEditTextDestination.setText(args.routeInfo.destinationName)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+
+        super.onDestroyView()
+    }
 }
