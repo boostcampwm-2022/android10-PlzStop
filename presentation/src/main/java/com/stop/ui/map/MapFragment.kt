eb2a0f809838854e457459680a910b6197acf52e
jeeminimini <jiminn821@naver.com>
2022-11-16 20:41:56 +0900

    feat : TMap sdk에서 클릭한 위치 정보 받아오기 구현

--- presentation/src/main/java/com/stop/ui/map/MapFragment.kt
+++ presentation/src/main/java/com/stop/ui/map/MapFragment.kt
@@ -5,14 +5,19 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
+import com.skt.tmap.TMapData
 import com.skt.tmap.TMapView
 import com.skt.tmap.overlay.TMapMarkerItem
 import com.stop.R
 import com.stop.databinding.FragmentMapBinding
 import com.stop.model.Location
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MapFragment : Fragment() {
     private var _binding: FragmentMapBinding? = null
@@ -46,6 +51,14 @@ class MapFragment : Fragment() {
                 Location(tMapPoint.latitude, tMapPoint.longitude)
             )
             tMapView.setCenterPoint(tMapPoint.latitude, tMapPoint.longitude)
+
+            CoroutineScope(Dispatchers.IO).launch {
+                val addressInfo = TMapData().reverseGeocoding(tMapPoint.latitude, tMapPoint.longitude, "A04").strFullAddress
+                launch(Dispatchers.Main) {
+                    Toast.makeText(requireContext(), addressInfo, Toast.LENGTH_SHORT).show()
+                }
+                Log.d("MainActivity","test하는중 $addressInfo")
+            }
         }
     }
 
