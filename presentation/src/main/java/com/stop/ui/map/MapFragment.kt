9ece54eb41a4a0884fc470145dbb693e5c9aa11b
khcho226 <khcho226@naver.com>
2022-11-16 19:52:07 +0900

    feat: initView, initTMap 함수 추가

--- presentation/src/main/java/com/stop/ui/map/MapFragment.kt
+++ presentation/src/main/java/com/stop/ui/map/MapFragment.kt
@@ -1,7 +1,6 @@
 package com.stop.ui.map
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,35 +10,55 @@ import com.stop.databinding.FragmentMapBinding
 
 class MapFragment : Fragment() {
     private var _binding: FragmentMapBinding? = null
-    private lateinit var tMapView: TMapView
     private val binding get() = _binding!!
+
+    private lateinit var tMapView: TMapView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
-
-        tMapView = TMapView(requireContext())
-        tMapView.setSKTMapApiKey(T_MAP_API_KEY)
-        tMapView.setOnMapReadyListener {
-            tMapView.mapType = TMapView.MapType.NIGHT
-        }
-        binding.layoutMap.addView(tMapView)
+        initBinding()
 
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+        initTMap()
+    }
 
+    private fun initBinding() {
+        // TODO lifecycleOwner, viewModel 설정
+    }
 
+    private fun initView() {
+        binding.imageViewCurrentLocation.setOnClickListener {
+
+        }
+
+        binding.imageViewCompassMode.setOnClickListener {
+
+        }
+    }
+
+    private fun initTMap() {
+        tMapView = TMapView(requireContext())
+        tMapView.setSKTMapApiKey(T_MAP_API_KEY)
+        tMapView.setOnMapReadyListener {
+            tMapView.mapType = TMapView.MapType.NIGHT
+            tMapView.zoomLevel = 16
+        }
+
+        binding.frameLayoutContainer.addView(tMapView)
     }
 
     override fun onDestroyView() {
-        super.onDestroyView()
         _binding = null
+        super.onDestroyView()
     }
 
     companion object {
