44b2973b966defe3a5355cf941aa492563dff8d5
khcho226 <khcho226@naver.com>
2022-11-16 21:20:04 +0900

    feat: User 위치 동기화 기능 구현

--- presentation/src/main/java/com/stop/ui/map/MapFragment.kt
+++ presentation/src/main/java/com/stop/ui/map/MapFragment.kt
@@ -7,10 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import com.skt.tmap.TMapGpsManager
 import com.skt.tmap.TMapView
+import com.skt.tmap.overlay.TMapMarkerItem
+import com.stop.R
 import com.stop.databinding.FragmentMapBinding
 
 class MapFragment : Fragment() {
@@ -67,7 +71,8 @@ class MapFragment : Fragment() {
 
         if (isTracking) {
             if (isLocationPermissionsGranted()) {
-
+                setManagerDefaultOptions(manager)
+                manager.setOnLocationChangeListener(onLocationChangeListener)
             }
         } else {
             manager.setOnLocationChangeListener(null)
@@ -86,6 +91,29 @@ class MapFragment : Fragment() {
 
         return permissions.any { permission ->
             checkSelfPermission(requireContext(), permission) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    private fun setManagerDefaultOptions(manager: TMapGpsManager) {
+        manager.minDistance = 2.5F
+        manager.provider = TMapGpsManager.PROVIDER_GPS
+        manager.openGps()
+        manager.provider = TMapGpsManager.PROVIDER_NETWORK
+        manager.openGps()
+    }
+
+    private val onLocationChangeListener = TMapGpsManager.OnLocationChangedListener { location ->
+        if (location != null) {
+            val marker = TMapMarkerItem().apply {
+                this.id = "marker_person_pin"
+                this.icon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_person_pin)?.toBitmap()
+                this.setTMapPoint(location.latitude, location.longitude)
+            }
+
+            tMapView.removeTMapMarkerItem("marker_person_pin")
+            tMapView.addTMapMarkerItem(marker)
+            tMapView.setCenterPoint(location.latitude, location.longitude, true)
+            tMapView.setLocationPoint(location.latitude, location.longitude)
         }
     }
 
