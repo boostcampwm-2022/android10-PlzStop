abdc4376142919cfa02422ca8d2c6e4b799ecac8
DoDoBest <av449@naver.com>
2022-11-16 15:27:16 +0900

    fix: T MAP API request 데이터 URL이 아닌 Body에 넣어서 요청

--- data/src/main/java/com/stop/data/remote/network/TmapApiService.kt
+++ data/src/main/java/com/stop/data/remote/network/TmapApiService.kt
@@ -2,14 +2,14 @@ package com.stop.data.remote.network
 
 import com.stop.data.remote.model.route.RouteResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.POST
-import retrofit2.http.QueryMap
 
 interface TmapApiService {
 
     @POST(TRANSPORT_URL)
     suspend fun getRoutes(
-        @QueryMap routeRequest: Map<String, String>
+        @Body routeRequest: Map<String, String>
     ): Response<RouteResponse>
 
     companion object {
