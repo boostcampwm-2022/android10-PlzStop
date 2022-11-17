57a1d9fe714a5a7b6aebe9bacdb81c9ca307b978
jeeminimini <jiminn821@naver.com>
2022-11-17 15:00:43 +0900

    feat : 화면 짧게 터치시, 정보 패널 지우기 구현
    - LongClick -> 패널 등장
    - 화면 이동 -> 패널 유지
    - Click -> 패널 삭제

--- presentation/src/main/java/com/stop/ui/map/MapFragment.kt
+++ presentation/src/main/java/com/stop/ui/map/MapFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment
 import com.skt.tmap.TMapData
 import com.skt.tmap.TMapPoint
 import com.skt.tmap.TMapView
+import com.skt.tmap.address.TMapAddressInfo
 import com.skt.tmap.overlay.TMapMarkerItem
 import com.stop.R
 import com.stop.databinding.FragmentMapBinding
@@ -18,12 +19,15 @@ import com.stop.model.Location
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 
 class MapFragment : Fragment() {
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var tMapView: TMapView
+    private var pressUpPoint = TMapPoint()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,6 +45,26 @@ class MapFragment : Fragment() {
         initView()
         initTMap()
         clickLocation()
+        clickMap()
+    }
+
+    private fun equals(point1: TMapPoint, point2: TMapPoint): Boolean {
+        return point1.latitude == point2.latitude && point2.longitude == point2.longitude
+    }
+
+    private fun clickMap() {
+        tMapView.setOnDisableScrollWithZoomLevelListener { _, centerPoint ->
+            if (equals(centerPoint, pressUpPoint)) {
+                binding.layoutPanel.visibility = binding.layoutPanel.visibility.run {
+                    if (this == View.GONE) {
+                        View.VISIBLE
+                    } else {
+                        View.GONE
+                    }
+                }
+            }
+            pressUpPoint = centerPoint
+        }
     }
 
     private fun clickLocation() {
@@ -50,23 +74,30 @@ class MapFragment : Fragment() {
                 R.drawable.ic_baseline_location_on_32,
                 Location(tMapPoint.latitude, tMapPoint.longitude)
             )
-            tMapView.setCenterPoint(tMapPoint.latitude, tMapPoint.longitude)
+            tMapView.setCenterPoint(tMapPoint.latitude, tMapPoint.longitude, true)
 
             setPanel(tMapPoint)
         }
     }
 
     private fun setPanel(tMapPoint: TMapPoint) {
-        CoroutineScope(Dispatchers.IO).launch {
-            val lotAddressInfo = TMapData().reverseGeocoding(tMapPoint.latitude, tMapPoint.longitude, LOT_ADDRESS_TYPE)
-            val roadAddressInfo = TMapData().reverseGeocoding(tMapPoint.latitude, tMapPoint.longitude, ROAD_ADDRESS_TYPE)
-
+        CoroutineScope(Dispatchers.Main).launch {
+            lateinit var lotAddressInfo: TMapAddressInfo
+            lateinit var roadAddressInfo: TMapAddressInfo
+            withContext(Dispatchers.IO) {
+                lotAddressInfo =
+                    TMapData().reverseGeocoding(tMapPoint.latitude, tMapPoint.longitude, LOT_ADDRESS_TYPE)
+                roadAddressInfo =
+                    TMapData().reverseGeocoding(tMapPoint.latitude, tMapPoint.longitude, ROAD_ADDRESS_TYPE)
+            }
             with(binding) {
                 textViewTitle.text = roadAddressInfo.strBuildingName
                 textViewLotAddress.text = lotAddressInfo.strFullAddress
-                textViewRoadAddress.text = roadAddressInfo.strFullAddress.split(roadAddressInfo.strBuildingName).first()
+                textViewRoadAddress.text =
+                    roadAddressInfo.strFullAddress.split(roadAddressInfo.strBuildingName).first()
             }
-
+            binding.layoutPanel.visibility = View.VISIBLE
+            pressUpPoint = TMapPoint(tMapPoint.latitude, tMapPoint.longitude)
         }
     }
 
