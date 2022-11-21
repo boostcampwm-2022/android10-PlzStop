d409b25aff9d9a83cf355cfd4be65bc9afc78ff3
DoDoBest <av449@naver.com>
2022-11-21 15:05:20 +0900

    refactor: 경기도 API XML에 대응되는 데이터 클래스 다시 생성

---
+++ data/src/main/java/com/stop/data/remote/model/route/gyeonggi/GyeonggiBusStation.kt
@@ -0,0 +1,25 @@
+package com.stop.data.remote.model.route.gyeonggi
+
+import com.tickaroo.tikxml.annotation.PropertyElement
+import com.tickaroo.tikxml.annotation.Xml
+
+@Xml(name = "busStationList")
+data class GyeonggiBusStation(
+    @PropertyElement(name = "stationId")
+    val stationId: Int,
+    @PropertyElement(name = "stationName")
+    val stationName: String,
+    @PropertyElement(name = "x")
+    val longitude: String,
+    @PropertyElement(name = "y")
+    val latitude: String,
+) {
+    fun toDomain(): com.stop.domain.model.route.gyeonggi.GyeonggiBusStation {
+        return com.stop.domain.model.route.gyeonggi.GyeonggiBusStation(
+            stationId = stationId,
+            stationName = stationName,
+            longitude = longitude,
+            latitude = latitude,
+        )
+    }
+}
