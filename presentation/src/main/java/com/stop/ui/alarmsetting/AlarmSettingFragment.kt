1e6f9caeb7fb38d5cd626bbc7c6dff77ef59df6e
koreatlwls <koreatlwls@gmail.com>
2022-11-21 15:21:48 +0900

    feat : 알람 설정 화면 일부 구현

--- presentation/src/main/java/com/stop/ui/alarmsetting/AlarmSettingFragment.kt
+++ presentation/src/main/java/com/stop/ui/alarmsetting/AlarmSettingFragment.kt
@@ -1,10 +1,12 @@
 package com.stop.ui.alarmsetting
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.stop.R
 import com.stop.databinding.FragmentAlarmSettingBinding
 
@@ -19,6 +21,44 @@ class AlarmSettingFragment : Fragment() {
         _binding = FragmentAlarmSettingBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonClick()
+        binding.numberPickerAlarmTime.minValue = 0
+        binding.numberPickerAlarmTime.maxValue = 60
+    }
+
+    private fun buttonClick() {
+        with(binding) {
+            textViewRouteContent.setOnClickListener {
+                if (textViewTransportContent.visibility == View.VISIBLE) {
+                    setTransportViewGone()
+                } else {
+                    setTransportViewVisible()
+                }
+            }
+        }
+    }
+
+    private fun setTransportViewGone() {
+        with(binding) {
+            TransitionManager.beginDelayedTransition(cardViewRoute, AutoTransition())
+            textViewTransportContent.visibility = View.GONE
+            textViewRouteContent.setCompoundDrawables(null, null, null, null)
+            textViewRouteContent.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_keyboard_arrow_down_24, 0)
+        }
+    }
+
+    private fun setTransportViewVisible() {
+        with(binding) {
+            TransitionManager.beginDelayedTransition(cardViewRoute, AutoTransition())
+            textViewTransportContent.visibility = View.VISIBLE
+            textViewRouteContent.setCompoundDrawables(null, null, null, null)
+            textViewRouteContent.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_keyboard_arrow_up_24, 0)
+        }
     }
 
     override fun onDestroyView() {
