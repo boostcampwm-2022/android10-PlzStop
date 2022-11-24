4e31ab00e78892d26da196b801a1edcfccb3c29e
DoTheBestMayB <send1684@gmail.com>
2022-11-24 17:38:55 +0900

    fix: Response 데이터 클래스에 빠트린 어노테이션 추가

--- domain/src/main/java/com/stop/domain/model/route/seoul/bus/GetBusLineResponse.kt
+++ domain/src/main/java/com/stop/domain/model/route/seoul/bus/GetBusLineResponse.kt
@@ -1,8 +1,10 @@
 package com.stop.domain.model.route.seoul.bus
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class GetBusLineResponse(
+    @Json(name = "msgBody")
     val lineIdMsgBody: LineIdMsgBody,
 )
--- domain/src/main/java/com/stop/domain/model/route/seoul/bus/GetBusStationArsIdResponse.kt
+++ domain/src/main/java/com/stop/domain/model/route/seoul/bus/GetBusStationArsIdResponse.kt
@@ -1,8 +1,10 @@
 package com.stop.domain.model.route.seoul.bus
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class GetBusStationArsIdResponse(
+    @Json(name = "msgBody")
     val arsIdMsgBody: ArsIdMsgBody,
 )
