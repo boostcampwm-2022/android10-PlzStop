85ad57d71b375648861eedb629a4bada59b9349b
koreatlwls <koreatlwls@gmail.com>
2022-11-29 11:48:02 +0900

    feat : 알람 데이터 알람코드 값 추가

--- domain/src/main/java/com/stop/domain/model/alarm/AlarmUseCaseItem.kt
+++ domain/src/main/java/com/stop/domain/model/alarm/AlarmUseCaseItem.kt
@@ -4,8 +4,9 @@ data class AlarmUseCaseItem(
     val startPosition: String,
     val endPosition: String,
     val routes: List<String>,
-    val lastTime: String,
-    val alarmTime: String,
+    val lastTime: String, // 막차 시간 -> 23:30:15 시분초
+    val alarmTime: Int, // 10분 전 알람 설정 -> 10
+    val alarmCode: Int, // 알람을 식별하기 위한 알람 ID
     val alarmMethod: Boolean,
     val isMission: Boolean,
 )
