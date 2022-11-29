9b53403fa025242c09d08ab0bc1ef280f69d6f7e
jeeminimini <jiminn821@naver.com>
2022-11-29 14:29:49 +0900

    feat : NowLocationRepositoryImpl의 getSubwayTrainNowLocation 구현

--- data/src/main/java/com/stop/data/model/nowlocation/SubwayTrainRealTimePositionRepositoryItem.kt
+++ data/src/main/java/com/stop/data/model/nowlocation/SubwayTrainRealTimePositionRepositoryItem.kt
@@ -1,5 +1,7 @@
 package com.stop.data.model.nowlocation
 
+import com.stop.domain.model.nowlocation.SubwayTrainRealTimePositionUseCaseItem
+
 data class SubwayTrainRealTimePositionRepositoryItem(
     val subwayId: String,
     val subwayName: String,
@@ -7,4 +9,13 @@ data class SubwayTrainRealTimePositionRepositoryItem(
     val stationName: String,
     val trainNumber: String,
     val trainStatus: String
-)
+) {
+    fun toUseCaseModel() = SubwayTrainRealTimePositionUseCaseItem(
+        subwayId = subwayId,
+        subwayName = subwayName,
+        stationId = stationId,
+        stationName = stationName,
+        trainNumber = trainNumber,
+        trainStatus = trainStatus
+    )
+}
--- data/src/main/java/com/stop/data/repository/NowLocationRepositoryImpl.kt
+++ data/src/main/java/com/stop/data/repository/NowLocationRepositoryImpl.kt
@@ -2,15 +2,20 @@ package com.stop.data.repository
 
 import com.stop.data.remote.source.nowlocation.NowLocationRemoteDataSource
 import com.stop.domain.model.nowlocation.BusInfoUseCaseItem
+import com.stop.domain.model.nowlocation.SubwayTrainRealTimePositionUseCaseItem
 import com.stop.domain.repository.NowLocationRepository
 import javax.inject.Inject
 
 class NowLocationRepositoryImpl @Inject constructor(
-    private val nowLocationRemoteDataSource: NowLocationRemoteDataSource
+    private val nowLocationRemoteDataSource: NowLocationRemoteDataSource,
 ) : NowLocationRepository{
 
     override suspend fun getBusNowLocation(busRouteId: String, order: Int): BusInfoUseCaseItem {
         return nowLocationRemoteDataSource.getBusNowLocation(busRouteId, order).toUseCaseModel()
+    }
+
+    override suspend fun getSubwayTrainNowLocation(trainNumber: Int): SubwayTrainRealTimePositionUseCaseItem {
+        return nowLocationRemoteDataSource.getSubwayTrainNowLocation(trainNumber).toUseCaseModel()
     }
 
 }
