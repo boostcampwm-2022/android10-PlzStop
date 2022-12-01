b5d0d22627017efded3ed16594b29d32f90f2944
jeeminimini <jiminn821@naver.com>
2022-12-01 13:47:18 +0900

    refactor : Marker name 변경

--- presentation/src/main/java/com/stop/ui/util/Marker.kt
+++ presentation/src/main/java/com/stop/ui/util/Marker.kt
@@ -7,21 +7,21 @@ object Marker {
     const val PLACE_MARKER = "place_marker"
     const val PLACE_MARKER_IMG = R.drawable.ic_point_marker
 
-    const val PERSON_MARKER = "marker_person_pin"
+    const val PERSON_MARKER = "person_marker"
     const val PERSON_MARKER_IMG = R.drawable.ic_person_marker
     const val PERSON_LINE = "person_line"
     const val PERSON_LINE_COLOR = Color.MAGENTA
 
     const val BOOKMARK_MARKER_IMG = R.drawable.ic_bookmark_marker
 
-    const val BUS_MARKER = "marker_bus_pin"
+    const val BUS_MARKER = "bus_marker"
     const val BUS_MARKER_IMG = R.drawable.ic_bus_marker
     const val BUS_LINE = "bus_line"
     const val BUS_LINE_COLOR = Color.BLUE
 
-    const val SUBWAY_MARKER = "marker_subway_pin"
+    const val SUBWAY_MARKER = "subway_marker"
     const val SUBWAY_MARKER_IMG = R.drawable.ic_subway_marker
-    const val SUBWAY_LINE = "bus_line"
+    const val SUBWAY_LINE = "subway_line"
     const val SUBWAY_LINE_COLOR = Color.BLUE
 
 }
