4cb5fa8a42266279d38fc4560f93c6fec245bcf3
DoDoBest <av449@naver.com>
2022-12-01 17:44:45 +0900

    temp: 집에 가서 하기( RecyclerView를 이용해서 소요 되는 시간 표시하기 )

---
+++ presentation/src/main/java/com/stop/ui/route/TimeLineAdapter.kt
@@ -0,0 +1,46 @@
+package com.stop.ui.route
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.stop.databinding.TimeLineItemBinding
+import com.stop.domain.model.route.tmap.custom.Route
+import com.stop.domain.model.route.tmap.custom.TransportRoute
+import com.stop.domain.model.route.tmap.custom.WalkRoute
+
+class TimeLineAdapter : ListAdapter<Route, TimeLineViewHolder>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimeLineViewHolder {
+        val binding = TimeLineItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return TimeLineViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TimeLineViewHolder, position: Int) {
+        holder.bind(getItem(position), holder.adapterPosition)
+    }
+
+    companion object {
+        private val diffUtil = object : DiffUtil.ItemCallback<Route>() {
+            override fun areItemsTheSame(oldRoute: Route, newRoute: Route): Boolean {
+                return false
+                return oldRoute.sectionTime == newRoute.sectionTime &&
+                        oldRoute.mode == newRoute.mode
+            }
+
+            override fun areContentsTheSame(oldRoute: Route, newRoute: Route): Boolean {
+                if (oldRoute is TransportRoute && newRoute is TransportRoute) {
+                    return oldRoute == newRoute
+                } else if (oldRoute is WalkRoute && newRoute is WalkRoute) {
+                    return oldRoute == newRoute
+                }
+                return false
+            }
+        }
+    }
+}
---
+++ presentation/src/main/java/com/stop/ui/route/TimeLineViewHolder.kt
@@ -0,0 +1,43 @@
+package com.stop.ui.route
+
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.stop.R
+import com.stop.databinding.TimeLineItemBinding
+import com.stop.domain.model.route.tmap.custom.MoveType
+import com.stop.domain.model.route.tmap.custom.Route
+
+class TimeLineViewHolder(
+    private val binding: TimeLineItemBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(route: Route, position: Int) {
+        val text = route.start.name + "   " +
+                binding.root.resources.getString(
+                    R.string.section_time,
+                    (route.sectionTime / 60).toInt().toString()
+                )
+        binding.textViewSectionTime.text = text
+
+
+        val imageSrc = when (route.mode) {
+            MoveType.BUS -> R.drawable.time_line_bus_16
+            MoveType.SUBWAY -> R.drawable.time_line_subway_16
+            MoveType.WALK, MoveType.TRANSFER -> {
+                if (position != 0) {
+                    binding.imageViewIcon.visibility = View.GONE
+                    return
+                }
+                R.drawable.time_line_directions_walk_16
+            }
+            else -> R.drawable.time_line_help_16
+        }
+
+
+        val drawable = ContextCompat.getDrawable(binding.root.context, imageSrc)
+            ?: throw IllegalArgumentException()
+
+        binding.imageViewIcon.setImageDrawable(drawable)
+    }
+}
