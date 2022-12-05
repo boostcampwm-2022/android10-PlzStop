351cf46a8998297c247cfbff4a54b00ba8fce4fe
DoDoBest <av449@naver.com>
2022-12-05 21:07:09 +0900

    fix: merge conflict 해결

--- presentation/src/main/java/com/stop/ui/route/ClickRouteViewModel.kt
+++ presentation/src/main/java/com/stop/ui/route/ClickRouteViewModel.kt
@@ -1,11 +1,13 @@
 package com.stop.ui.route
 
 import androidx.lifecycle.ViewModel
+import com.stop.domain.model.route.TransportLastTime
 import com.stop.domain.model.route.tmap.custom.Itinerary
 
 class ClickRouteViewModel : ViewModel() {
 
     var clickRoute : Itinerary? = null
-    var lastTime = "23:30:00"
+    var lastTimes: List<TransportLastTime?>? = null
+    val lastTime = "23:30:00"
 
 }
