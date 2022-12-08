3306f3ed1511e468596ed6464c8ee7aa6eb5c62b
jeeminimini <jiminn821@naver.com>
2022-12-08 15:35:01 +0900

    style : 테스트용 하드코딩 수정

--- presentation/src/main/java/com/stop/ui/alarmsetting/AlarmSettingViewModel.kt
+++ presentation/src/main/java/com/stop/ui/alarmsetting/AlarmSettingViewModel.kt
@@ -68,7 +68,7 @@ class AlarmSettingViewModel @Inject constructor(
     }
 
     fun callAlarm(time: String) {
-        alarmFunctions.callAlarm("01:00:00", alarmTime.value ?: 0, ALARM_CODE)
+        alarmFunctions.callAlarm(time, alarmTime.value ?: 0, ALARM_CODE)
     }
 
     fun cancelAlarm() {
